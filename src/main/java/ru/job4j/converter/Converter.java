8276a538340c9b852e9f6cede674eef0a2012e65
Aleksandrleavegame <revin90@yandex.ru>
2021-12-19 23:18:19 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -21,29 +21,36 @@ public class Converter {
         return rs1;
     }
 
-    public static double dollarToEuro(double value) {
-        double rs1 = value * 1.15;
-        return rs1;
-    }
-
-    public static double euroToDollar(double value) {
-        double rs1 = value / 1.15;
-        return rs1;
-    }
-
     public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        in = 120;
+        expected = 2;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("120 rubles are 2. Test result : " + passed);
+        in = 240;
+        expected = 16800;
+        out = Converter.euroToRuble(in);
+        passed = expected == out;
+        System.out.println("240 euro are 16800. Test result : " + passed);
+        in = 450;
+        expected = 27000;
+        out = Converter.dollarToRuble(in);
+        passed = expected == out;
+        System.out.println("450 dollars are 27000. Test result : " + passed);
+
         float euroRuble = Converter.rubleToEuro(140);
-        float dollarRuble = Converter.rubleToDollar(350);
+        float dollarRuble = Converter.rubleToDollar(120);
         float rubleEuro = Converter.euroToRuble(240);
         float rubleDollar = Converter.dollarToRuble(450);
-        double dollarEuro = Converter.euroToDollar(500);
-        double euroDollar = Converter.dollarToEuro(600);
         System.out.println("140 rubles are " + euroRuble + " euro.");
-        System.out.println("350 rubles are " + dollarRuble + " dollars");
+        System.out.println("120 rubles are " + dollarRuble + " dollars");
         System.out.println("240 euro are " + rubleEuro + " rubles");
         System.out.println("450 dollars are " + rubleDollar + " rubles");
-        System.out.println("500 euro are " + dollarEuro + " dollars");
-        System.out.println("600 dollars are " + euroDollar + " euro");
 
     }
 }
