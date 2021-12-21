36733af96430717dad3395a295967cf8e13dad17
Aleksandrleavegame <revin90@yandex.ru>
2021-12-21 19:53:41 +0300

    4. Тесты для стороны прямоугольника. [#159460 #242360]

--- src/main/java/ru/job4j/probapera/Doubletrouble.java
+++ src/main/java/ru/job4j/probapera/Doubletrouble.java
@@ -1,13 +1,11 @@
 package ru.job4j.probapera;
 
 public class Doubletrouble {
-    public static void main (String[] args) {
+    @SuppressWarnings("checkstyle:MethodParamPad")
+    public static void main(String[] args) {
         int a = 123;
         int b = 411;
         int c = a + b;
         System.out.println(c);
-
-
-
     }
 }
--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -30,6 +29,15 @@ public class SqAreaTest {
         int expected = 4;
         int p = 8;
         int k = 1;
+        double out = SqArea.square(p, k);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP9K2then4dot5() {
+        double expected = 4.5;
+        int p = 9;
+        int k = 2;
         double out = SqArea.square(p, k);
         assertEquals(expected, out, 0.01);
     }
