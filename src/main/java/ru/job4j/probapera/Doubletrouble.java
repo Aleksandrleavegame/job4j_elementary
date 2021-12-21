96329e0b07c158a72ea5d5db0849f53161f7b744
Aleksandrleavegame <revin90@yandex.ru>
2021-12-21 19:26:01 +0300

    proverka

---
+++ src/main/java/ru/job4j/probapera/Doubletrouble.java
@@ -0,0 +1,13 @@
+package ru.job4j.probapera;
+
+public class Doubletrouble {
+    public static void main (String[] args) {
+        int a = 123;
+        int b = 411;
+        int c = a + b;
+        System.out.println(c);
+
+
+
+    }
+}
