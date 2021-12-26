ade521202a524b9221b88c64906127c6c1a967a9
Aleksandrleavegame <revin90@yandex.ru>
2021-12-26 15:41:25 +0300

    4.2. Тернарное сравнение [#189 #244091]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,28 +1,8 @@
 package ru.job4j;
 
 public class Echo {
-    public static boolean ecj(int a, int b, int c) {
-        boolean rsl = b > c;
-        if (a > b) { rsl = a > c;
-        }
-        if (a > c) {
-            System.out.println(a);
-        }
-        else {
-            System.out.println(b);
-        }
-        if (b > c) {
-            System.out.println(b);
-        }
-        else {
-            System.out.println(c);
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) {
-        Echo.ecj( 1, 2, 3);
+        System.out.println("Hello World!");
     }
 }
 
